c05becb8fb2de88bb8ff410ae47d39007ad3b91c
k1n0r4 <93029135+k1n0r4@users.noreply.github.com>
2024-03-11 18:03:36 +0000

    Build: (5a4643d) Delete public/assets/blog/welcome/welcome.jpg

---
+++ _next/static/chunks/app/layout-aaf1f6929394a639.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5951:function(n,e,t){Promise.resolve().then(t.t.bind(t,3385,23)),Promise.resolve().then(t.t.bind(t,4547,23))},3385:function(){},4547:function(){}},function(n){n.O(0,[971,69,744],function(){return n(n.s=5951)}),_N_E=n.O()}]);
